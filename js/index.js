31bc2feb1449438f63e14aba496016b8af5876fd
Ephraim19 <machayoephraim@gmail.com>
2021-12-09 09:34:45 +0300

    Added js event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
-console.log("active")
+var input = document.getElementById("submitButton");
+input.addEventListener("click", submittedValues, true);
+
+function submittedValues(event) {
+  event.preventDefault();
+  alert("eph");
+};
