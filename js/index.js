7fd18aae566dd4b90f1c7544c8956fdc987ec3f3
Ephraim19 <machayoephraim@gmail.com>
2021-12-09 09:46:47 +0300

    Added logical operators to check if the date of birth is correct

--- js/index.js
+++ js/index.js
@@ -6,5 +6,9 @@ function submittedValues(event) {
   let day = document.getElementById("day").value;
   let month = document.getElementById("month").value;
   let year = document.getElementById("year").value;
-  alert(day);
+  if (day <= 31 && month <= 12 && year <= 2021){
+      confirm(month)
+  }else {
+      alert("Ensure your date of birth is correct please")
+  }
 }
