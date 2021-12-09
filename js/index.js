9a0935f585ea97e876d13065c7adea2d5f4764e6
Ephraim19 <machayoephraim@gmail.com>
2021-12-09 21:34:53 +0300

    Created index.css

--- js/index.js
+++ js/index.js
@@ -6,6 +6,8 @@ function submittedValues(event) {
   let day = document.getElementById("day").value;
   let month = document.getElementById("month").value;
   let year = document.getElementById("year").value;
+  let gender = document.getElementsByName("gender");
+
   if (day > 0 && day <= 31) {
     if (month > 0 && month <= 12) {
       let CC = year.slice(0, 2);
@@ -14,11 +16,12 @@ function submittedValues(event) {
       let DD = day;
       let answer =
         (CC / 4 - 2 * CC - 1 + (5 * YY) / 4 + (26 * (MM + 1)) / 10 + DD) % 7;
-      console.log(answer);
+
+      alert("your Akan name is " + answer);
     } else {
       alert("Ensure your month of birth is correct please");
     }
-  }else {
-    alert("Ensure your day of birth is correct please"); 
+  } else {
+    alert("Ensure your day of birth is correct please");
   }
 }
