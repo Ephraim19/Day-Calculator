c4d970b9ad678549e3cbd7ad8f89a5554f91c831
Ephraim19 <machayoephraim@gmail.com>
2021-12-09 10:38:03 +0300

    Added a description to the web app

--- js/index.js
+++ js/index.js
@@ -6,9 +6,19 @@ function submittedValues(event) {
   let day = document.getElementById("day").value;
   let month = document.getElementById("month").value;
   let year = document.getElementById("year").value;
-  if (day <= 31 && month <= 12 && year <= 2021){
-      confirm(month)
+  if (day > 0 && day <= 31) {
+    if (month > 0 && month <= 12) {
+      let CC = year.slice(0, 2);
+      let YY = year.slice(2, 4);
+      let MM = month;
+      let DD = day;
+      let answer =
+        (CC / 4 - 2 * CC - 1 + (5 * YY) / 4 + (26 * (MM + 1)) / 10 + DD) % 7;
+      console.log(answer);
+    } else {
+      alert("Ensure your month of birth is correct please");
+    }
   }else {
-      alert("Ensure your date of birth is correct please")
+    alert("Ensure your day of birth is correct please"); 
   }
 }
