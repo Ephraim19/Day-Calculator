2d1ed45114aeb6894e9db7b0dbd3059ae028a12d
Ephraim19 <machayoephraim@gmail.com>
2021-12-10 08:48:10 +0300

    Added a queryselector for the gender radio form

--- js/index.js
+++ js/index.js
@@ -6,16 +6,17 @@ function submittedValues(event) {
   var day = document.getElementById("day").value;
   var month = document.getElementById("month").value;
   var year = document.getElementById("year").value;
-  var gender = document.getElementsByName("gender");
+  var gender = document.querySelector('input[name="gender"]:checked').value
 
+  //var female = document.getElementById("female").checked
+  console.log(gender)
   if (day > 0 && day <= 31) {
     if (month > 0 && month <= 12) {
       var CC = year.slice(0, 2);
       var YY = year.slice(2, 4);
       var MM = month;
       var DD = day;
-      var answer =
-        (CC / 4 - 2 * CC - 1 + (5 * YY) / 4 + (26 * (MM + 1)) / 10 + DD) % 7;
+      userName()
     } else {
       alert("Ensure your month of birth is correct please");
     }
@@ -25,6 +26,7 @@ function submittedValues(event) {
 }
 
 function userName(answer, gender) {
+  var answer = 3
   var maleNames = [
     "Kwasi",
     "Kwadwo",
@@ -37,35 +39,35 @@ function userName(answer, gender) {
   var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
   if (gender === "male") {
     if (answer === 0) {
-      alert("your Akan name is " + maleNames[0]);
+      alert("Your Akan name is " + maleNames[0]);
     } else if (answer === 1) {
-      alert("your Akan name is " + maleNames[1]);
+      alert("Your Akan name is " + maleNames[1]);
     } else if (answer === 2) {
-      alert("your Akan name is " + maleNames[2]);
+      alert("Your Akan name is " + maleNames[2]);
     } else if (answer === 3) {
-      alert("your Akan name is " + maleNames[3]);
+      alert("Your Akan name is " + maleNames[3]);
     } else if (answer === 4) {
-      alert("your Akan name is " + maleNames[4]);
+      alert("Your Akan name is " + maleNames[4]);
     } else if (answer === 5) {
-      alert("your Akan name is " + maleNames[5]);
+      alert("Your Akan name is " + maleNames[5]);
     } else if (answer === 6) {
-      alert("your Akan name is " + maleNames[6]);
+      alert("Your Akan name is " + maleNames[6]);
     }
   } else {
     if (answer === 0) {
-      alert("your Akan name is " + femaleNames[0]);
+      alert("Your Akan name is " + femaleNames[0]);
     } else if (answer === 1) {
-      alert("your Akan name is " + femaleNames[1]);
+      alert("Your Akan name is " + femaleNames[1]);
     } else if (answer === 2) {
-      alert("your Akan name is " + femaleNames[2]);
+      alert("Your Akan name is " + femaleNames[2]);
     } else if (answer === 3) {
-      alert("your Akan name is " + femaleNames[3]);
+      alert("Your Akan name is " + femaleNames[3]);
     } else if (answer === 4) {
-      alert("your Akan name is " + femaleNames[4]);
+      alert("Your Akan name is " + femaleNames[4]);
     } else if (answer === 5) {
-      alert("your Akan name is " + femaleNames[5]);
+      alert("Your Akan name is " + femaleNames[5]);
     } else if (answer === 6) {
-      alert("your Akan name is " + femaleNames[6]);
+      alert("Your Akan name is " + femaleNames[6]);
     }
   }
 }
