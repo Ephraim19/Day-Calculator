93484bf4f4d3a3dc848ffdf254dd59abb2648097
Ephraim19 <machayoephraim@gmail.com>
2021-12-15 10:25:30 +0300

    added day of the week in the alert message

--- js/index.js
+++ js/index.js
@@ -39,37 +39,39 @@ function submittedValues(event) {
           "Afua",
           "Ama",
         ];
+        var dayOfWeek = ['Sunday', 'Monday', 'Tuesday' ,'Wednesday','Thursday','Friday','Saturday']
+
         if (gender === "male") {
           if (answer === 0) {
-            alert("Your Akan name is " + maleNames[0]);
+            alert("You were born on " + dayOfWeek[0] + " and your Akan name is " + maleNames[0]);
           } else if (answer === 1) {
-            alert("Your Akan name is " + maleNames[1]);
+            alert("You were born on " + dayOfWeek[1] + " and your Akan name is " + maleNames[1]);
           } else if (answer === 2) {
-            alert("Your Akan name is " + maleNames[2]);
+            alert("You were born on " + dayOfWeek[2] + " and your Akan name is " + maleNames[2]);
           } else if (answer === 3) {
-            alert("Your Akan name is " + maleNames[3]);
+            alert("You were born on " + dayOfWeek[3] + " and your Akan name is " + maleNames[3]);
           } else if (answer === 4) {
-            alert("Your Akan name is " + maleNames[4]);
+            alert("You were born on " + dayOfWeek[4] + " and your Akan name is " + maleNames[4]);
           } else if (answer === 5) {
-            alert("Your Akan name is " + maleNames[5]);
+            alert("You were born on " + dayOfWeek[5] + " and your Akan name is " + maleNames[5]);
           } else if (answer === 6) {
-            alert("Your Akan name is " + maleNames[6]);
+            alert("You were born on " + dayOfWeek[6] + " and your Akan name is " + maleNames[6]);
           }
         } else {
           if (answer === 0) {
-            alert("Your Akan name is " + femaleNames[0]);
+            alert("You were born on " + dayOfWeek[0] + " and your Akan name is " + femaleNames[0]);
           } else if (answer === 1) {
-            alert("Your Akan name is " + femaleNames[1]);
+            alert("You were born on " + dayOfWeek[1] + " and your Akan name is " + femaleNames[1]);
           } else if (answer === 2) {
-            alert("Your Akan name is " + femaleNames[2]);
+            alert("You were born on " + dayOfWeek[2] + " and your Akan name is " + femaleNames[2]);
           } else if (answer === 3) {
-            alert("Your Akan name is " + femaleNames[3]);
+            alert("You were born on " + dayOfWeek[3] + " and your Akan name is " + femaleNames[3]);
           } else if (answer === 4) {
-            alert("Your Akan name is " + femaleNames[4]);
+            alert("You were born on " + dayOfWeek[4] + " and your Akan name is " + femaleNames[4]);
           } else if (answer === 5) {
-            alert("Your Akan name is " + femaleNames[5]);
+            alert("You were born on " + dayOfWeek[5] + " and your Akan name is " + femaleNames[5]);
           } else if (answer === 6) {
-            alert("Your Akan name is " + femaleNames[6]);
+            alert("You were born on " + dayOfWeek[6] + " and your Akan name is " + femaleNames[6]);
           }
         }
       } else {
